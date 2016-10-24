fbe860d0b8dd36634cb19353ba5a38e262040f72
thomas <thomas.schmelzer@gmail.com>
2016-10-24 08:07:59 +0200

    new notebooks

--- books/talk/config.py
+++ books/talk/config.py
@@ -1,15 +1,9 @@
-import os
-BASEPATH = os.path.dirname(os.path.dirname(__file__))
+import matplotlib
+import matplotlib.pyplot as plt
+plt.style.use = 'default'
 
-from pandas import options
-options.display.mpl_style = 'default'
-
-def config_mosek():
-    print("Set MOSEKLM_LICENSE_FILE environment variable")    
-    os.environ["MOSEKLM_LICENSE_FILE"] = os.path.join(BASEPATH, "license", "mosek.lic")
     
 def config_configManager():
-    print("Update ConfigManager")
     from notebook.services.config import ConfigManager
     cm = ConfigManager()
     cm.update('livereveal', {
@@ -21,8 +15,6 @@ def config_configManager():
 
 
 def config_matplotlib():
-    import matplotlib.pyplot as plt
-
     params = {'legend.fontsize': 'large', 
               'lines.linewidth':3, 
               'font.size':16, 
